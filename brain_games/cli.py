b52259d278b55a616797231f1188a625ef1c3b85
xsl1px <61144707+xsl1px@users.noreply.github.com>
2020-05-16 17:12:46 +0300

    Update cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,18 @@
+"""
+This is an example script.
+
+It seems that it has to have THIS docstring with a summary line, a blank line
+and sume more text like here. Wow.
+"""
 import prompt
 
 
 def welcome_user():
+    """
+    This is an example script.
+
+    It seems that it has to have THIS docstring with a summary line, a blank line
+    and sume more text like here. Wow.
+    """
     name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(name))
